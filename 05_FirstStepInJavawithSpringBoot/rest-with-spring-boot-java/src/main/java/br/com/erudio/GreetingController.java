5c7179516739f8097af9a16be94644cf2ef037b6
rogersds <rogger_souza@hotmail.com>
2024-07-13 14:49:31 -0300

    lfirst Steps in Java with Spring boot3

---
+++ 05_FirstStepInJavawithSpringBoot/rest-with-spring-boot-java/src/main/java/br/com/erudio/GreetingController.java
@@ -0,0 +1,26 @@
+package br.com.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	private static final String template = "Hello, %s!";
+	private static final AtomicLong counter = new AtomicLong();
+
+	
+	@RequestMapping("/greeting")
+	
+	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	    long id = counter.incrementAndGet();
+	    String message = String.format(template, name);
+	    return new Greeting(id, message);
+	}
+	
+	
+	
+}
